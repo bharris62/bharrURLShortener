6d4a5ff078603428c9cb4565235ec355721bfebc
bharris62 <blakebharris@gmail.com>
2017-06-11 15:12:32 -0500

    first commit

---
+++ shortener/utils.py
@@ -0,0 +1,16 @@
+import random
+import string
+
+def code_generator(size=6, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+def create_shortcode(instance, size=6):
+    new_code = code_generator(size=size)
+
+    Blass = instance.__class__
+    qs_exists = Blass.objects.filter(shortcode=new_code).exists()
+
+    if qs_exists:
+        return create_shortcode(size=size)
+
+    return new_code
